abd0ac41fd139b6eafe03e29f71bda320edf3670
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2021-10-20 10:55:47 -0300

    Uses spaces instead of tabs.

--- utils/migrate-sponsors.js
+++ utils/migrate-sponsors.js
@@ -10,24 +10,24 @@ const filter = 'flatten | map({ tier } + (.sponsors | to_entries | map({ order: 
 jq.run(filter, sponsorsFilepath, {})
   .then((output) => {
     try {
-	    let sponsors = JSON.parse(output);
-	    let promises = sponsors.map(sponsor => {
-	    	let filename = kebabCase(sponsor.name)
-	    	return fs.writeFile(`${sponsorsPath}/${filename}.json`, JSON.stringify(sponsor, null, 2), function(err) {
-				    if (err) {
-				        return console.log('error creating the sponsor files', err)
-				    }
-				    console.log('sponsor files created')
-				})
-	    })
-	    Promise.all(promises)
-	      .then(() => {
-	        console.log('sponsors files created')
-	      })
-	      .catch(err => {
-	        console.log('error creating the sponsors files', err)
-	      })
-		} catch (err) {
+      let sponsors = JSON.parse(output);
+      let promises = sponsors.map(sponsor => {
+        let filename = kebabCase(sponsor.name)
+        return fs.writeFile(`${sponsorsPath}/${filename}.json`, JSON.stringify(sponsor, null, 2), function(err) {
+            if (err) {
+                return console.log('error creating the sponsor files', err)
+            }
+            console.log('sponsor files created')
+        })
+      })
+      Promise.all(promises)
+        .then(() => {
+          console.log('sponsors files created')
+        })
+        .catch(err => {
+          console.log('error creating the sponsors files', err)
+        })
+    } catch (err) {
         console.log('error parsing sponsors json', err)
     }
   })
